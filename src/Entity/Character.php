59f154a8d395a0673a9c046b55d3e7468c96dd34
LE FORT François <slayer171.flf@gmail.com>
2024-03-12 17:05:11 +0100

    Add first entity : character

---
+++ src/Entity/Character.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CharacterRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CharacterRepository::class)]
+#[ORM\Table(name: '`character`')]
+class Character
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $eveid = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getEveid(): ?int
+    {
+        return $this->eveid;
+    }
+
+    public function setEveid(?int $eveid): static
+    {
+        $this->eveid = $eveid;
+
+        return $this;
+    }
+}
