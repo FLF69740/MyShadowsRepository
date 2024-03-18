1f48d06b7640520a607dbf216b6881cce3714bd6
LE FORT François <slayer171.flf@gmail.com>
2024-03-18 15:18:15 +0100

    Create nelmio bundle and yaml

--- src/Controller/CharacterController.php
+++ src/Controller/CharacterController.php
@@ -8,9 +8,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Serializer\SerializerInterface;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use Nelmio\ApiDocBundle\Annotation\Security;
+use OpenApi\Annotations as OA;
 
 class CharacterController extends AbstractController
 {
+    /**
+     * @OA\Tag(name="Character")
+     * 
+     * @param CharacterRepository $characterRepository
+     * @param SerializerInterface $serializer
+     * @param Request $request
+     * @return JsonResponse
+     */
     #[Route('/api/characters', name: 'app_character', methods:['GET'])]
     public function getCharacters(
         SerializerInterface $serializer,
@@ -29,6 +40,14 @@ class CharacterController extends AbstractController
         );
     }
 
+    /**
+     * @OA\Tag(name="Character")
+     * 
+     * @param CharacterRepository $characterRepository
+     * @param SerializerInterface $serializer
+     * @param Request $request
+     * @return JsonResponse
+     */
     #[Route('/api/characters/{id}', name: 'single_character', methods:['GET'])]
     public function getSingleCharacter(
         SerializerInterface $serializer,
